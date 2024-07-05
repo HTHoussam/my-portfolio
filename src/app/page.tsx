3e5c5c34cbcd69eb18cf303c95589593c9f7bf53
Houssam Habbat <houssamhabbat0@gmail.com>
2024-07-05 19:02:33 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+      {/* header */}
+      <div className="flex justify-between p-2 w-full">
+        <div>image</div>
+        <div>Navigation here </div>
+      </div>
+    </main>
+  );
+}
