07d7b4c2707bbae04dc70b0905becb5df4365d4f
Houssam HT <houssamhabbat0@gmail.com>
2024-08-12 19:59:07 +0100

    fix: email sending test1

--- src/app/action.ts
+++ src/app/action.ts
@@ -3,44 +3,47 @@
 import contactMeSchema, { ContactMeSchemaType } from '@/lib/validationSchema';
 import nodemailer from 'nodemailer';
 
-export async function emailme(formData: ContactMeSchemaType) {
-	const result = contactMeSchema.safeParse(formData);
-	const { data, success } = result;
-	if (success && data) {
-		const { email, name, message, subject } = data;
+export async function emailme(formData: ContactMeSchemaType): Promise<void> {
+	return new Promise(async (resolve, reject) => {
+		const result = contactMeSchema.safeParse(formData);
+		const { data, success } = result;
 
-		const transporter = nodemailer.createTransport({
-			host: 'mail.infomaniak.com',
-			port: 465,
-			secure: true, // true for 465, false for other ports
-			auth: {
-				user: process.env.EMAIL_USERNAME,
-				pass: process.env.EMAIL_PASSWORD,
-			},
-		});
+		if (success && data) {
+			const { email, name, message, subject } = data;
 
-		// Define the email options
-		const mailOptions = {
-			from: {
-				name: name,
-				address: process.env.EMAIL_USERNAME ?? '',
-			},
-			replyTo: email,
-			to: process.env.EMAIL_USERNAME ?? '',
-			subject: `${subject}`,
-			text: `Name: ${formData.name}\nEmail: ${email}\nMessage: ${formData.message}`,
-		};
-		try {
-			transporter.sendMail(mailOptions, (error, info) => {
-				if (error) {
-					console.error('Error sending email:', error);
-				} else {
-					console.log('Email sent:', info.response);
-				}
+			const transporter = nodemailer.createTransport({
+				host: 'mail.infomaniak.com',
+				port: 465,
+				secure: true, // true for 465, false for other ports
+				auth: {
+					user: process.env.EMAIL_USERNAME,
+					pass: process.env.EMAIL_PASSWORD,
+				},
 			});
-		} catch (error: any) {
-			throw Error('Error sending email:', error);
+
+			// Define the email options
+			const mailOptions = {
+				from: {
+					name: name,
+					address: process.env.EMAIL_USERNAME ?? '',
+				},
+				replyTo: email,
+				to: process.env.EMAIL_USERNAME ?? '',
+				subject: `${subject}`,
+				text: `Name: ${formData.name}\nEmail: ${email}\nMessage: ${formData.message}`,
+			};
+
+			try {
+				// Send the email
+				await transporter.sendMail(mailOptions);
+				console.log('Email sent successfully');
+				resolve(); // Resolve the promise on success
+			} catch (error: any) {
+				console.error('Error sending email:', error);
+				reject(new Error(`Error sending email: ${error.message}`)); // Reject the promise on error
+			}
+		} else {
+			reject(new Error('Validation failed')); // Reject if validation fails
 		}
-		// Send the email
-	}
+	});
 }
--- src/app/components/contact-me.tsx
+++ src/app/components/contact-me.tsx
@@ -1,136 +1,140 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import contactMeSchema, { ContactMeSchemaType } from "@/lib/validationSchema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { SendHorizontal } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { emailme } from "../action";
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import contactMeSchema, { ContactMeSchemaType } from '@/lib/validationSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SendHorizontal } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { emailme } from '../action';
 const ContactMe = () => {
-  const form = useForm<ContactMeSchemaType>({
-    defaultValues: {
-      name: "",
-      subject: "",
-      email: "",
-      message: "",
-    },
-    mode: "onChange",
-    resolver: zodResolver(contactMeSchema),
-  });
-  async function onSubmit(data: ContactMeSchemaType) {
-    await emailme(data);
-  }
+	const form = useForm<ContactMeSchemaType>({
+		defaultValues: {
+			name: '',
+			subject: '',
+			email: '',
+			message: '',
+		},
+		mode: 'onChange',
+		resolver: zodResolver(contactMeSchema),
+	});
+	async function onSubmit(data: ContactMeSchemaType) {
+		try {
+			await emailme(data);
+			console.log('Email sent successfully!');
+		} catch (error) {
+			console.error('Failed to send email:', error);
+		}
+	}
 
-  return (
-    <div className="flex flex-col mt-8" id="message__me">
-      <h3 className="text-4xl font-bold mb-6 text-left capitalize bg-gradient-to-r from-primary to-slate-50 bg-clip-text text-transparent">
-        Message me
-      </h3>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit, (error) => {
-            console.log("error", error);
-          })}
-        >
-          <div className="flex flex-col w-full items-center">
-            <div className="grid grid-cols-1 gap-4 lg:gap-8 w-full max-w-2xl">
-              <div>
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem className="text-left">
-                      <FormLabel className="capitalize">name</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="text-muted-foreground"
-                          placeholder="john doe"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div>
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem className="text-left">
-                      <FormLabel className="capitalize">email</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="text-muted-foreground"
-                          placeholder="john@doe.com"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div>
-                <FormField
-                  control={form.control}
-                  name="subject"
-                  render={({ field }) => (
-                    <FormItem className="text-left">
-                      <FormLabel className="capitalize">subject</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="text-muted-foreground"
-                          placeholder="shadcn"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div>
-                <FormField
-                  control={form.control}
-                  name="message"
-                  render={({ field }) => (
-                    <FormItem className="text-left">
-                      <FormLabel className="capitalize">Message</FormLabel>
-                      <FormControl>
-                        <Textarea
-                          className="text-muted-foreground"
-                          placeholder="message here ..."
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div>
-                <Button type="submit" variant={"outline"}>
-                  Send message
-                  <SendHorizontal className="ml-2 h-4 w-4" />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </form>
-      </Form>
-    </div>
-  );
+	return (
+		<div className='flex flex-col mt-8' id='message__me'>
+			<h3 className='text-4xl font-bold mb-6 text-left capitalize bg-gradient-to-r from-primary to-slate-50 bg-clip-text text-transparent'>
+				Message me
+			</h3>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit, (error) => {
+						console.log('error', error);
+					})}>
+					<div className='flex flex-col w-full items-center'>
+						<div className='grid grid-cols-1 gap-4 lg:gap-8 w-full max-w-2xl'>
+							<div>
+								<FormField
+									control={form.control}
+									name='name'
+									render={({ field }) => (
+										<FormItem className='text-left'>
+											<FormLabel className='capitalize'>name</FormLabel>
+											<FormControl>
+												<Input
+													className='text-muted-foreground'
+													placeholder='john doe'
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+							<div>
+								<FormField
+									control={form.control}
+									name='email'
+									render={({ field }) => (
+										<FormItem className='text-left'>
+											<FormLabel className='capitalize'>email</FormLabel>
+											<FormControl>
+												<Input
+													className='text-muted-foreground'
+													placeholder='john@doe.com'
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+							<div>
+								<FormField
+									control={form.control}
+									name='subject'
+									render={({ field }) => (
+										<FormItem className='text-left'>
+											<FormLabel className='capitalize'>subject</FormLabel>
+											<FormControl>
+												<Input
+													className='text-muted-foreground'
+													placeholder='shadcn'
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+							<div>
+								<FormField
+									control={form.control}
+									name='message'
+									render={({ field }) => (
+										<FormItem className='text-left'>
+											<FormLabel className='capitalize'>Message</FormLabel>
+											<FormControl>
+												<Textarea
+													className='text-muted-foreground'
+													placeholder='message here ...'
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+							<div>
+								<Button type='submit' variant={'outline'}>
+									Send message
+									<SendHorizontal className='ml-2 h-4 w-4' />
+								</Button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</Form>
+		</div>
+	);
 };
 export default ContactMe;
