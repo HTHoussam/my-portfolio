7434d400544858e5f85b25ccc8b70e1f83f071de
Houssam Habbat <houssamhabbat0@gmail.com>
2024-10-20 18:00:51 +0100

    fix: update img and optmize it

--- src/app/components/core/resume-download-button.tsx
+++ src/app/components/core/resume-download-button.tsx
@@ -1,26 +1,25 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Download } from "lucide-react";
+'use client';
+import { Button } from '@/components/ui/button';
+import { Download } from 'lucide-react';
 const ResumeDownloadButton = () => {
-  const handleDownload = () => {
-    console.log("im here1");
-    const link = document.createElement("a");
-    link.href = "/assets/pdf/Houssam_Habbat_Resume.pdf"; // Path to your resume in the public directory
-    link.download = "Houssam_Habbat_Resume.pdf"; // The name of the file to be downloaded
-    link.click();
-  };
+	const handleDownload = () => {
+		console.log('im here1');
+		const link = document.createElement('a');
+		link.href = '/assets/pdf/Houssam_Habbat_Resume.pdf'; // Path to your resume in the public directory
+		link.download = 'Houssam_Habbat_Resume.pdf'; // The name of the file to be downloaded
+		link.click();
+	};
 
-  return (
-    <Button
-      onClick={handleDownload}
-      className="relative overflow-hidden bg-gradient-to-r from-purple-500 to-blue-500 text-white group"
-    >
-      <span className="absolute inset-0 bg-gradient-to-r from-blue-500 to-purple-500 transition-transform transform translate-x-full group-hover:translate-x-0 ease-in-out duration-300"></span>
-      <span className="relative flex items-center">
-        <Download className="mr-2 h-4 w-4" />
-        Download Resume
-      </span>
-    </Button>
-  );
+	return (
+		<Button
+			onClick={handleDownload}
+			className='relative overflow-hidden bg-gradient-to-r from-purple-500 to-blue-500 text-white group'>
+			<span className='absolute inset-0 bg-gradient-to-r from-blue-500 to-purple-500 transition-transform transform translate-x-full group-hover:translate-x-0 ease-in-out duration-300'></span>
+			<span className='relative flex items-center'>
+				<Download className='mr-2 h-4 w-4' />
+				Download Resume
+			</span>
+		</Button>
+	);
 };
 export default ResumeDownloadButton;
